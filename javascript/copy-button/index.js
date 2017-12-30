bc9d6e3de0510a21c84852355308f9f703d91be7
JacobTDC <jacob.story.JS@gmail.com>
2017-12-30 12:04:07 -0600

    Update index.js

--- javascript/copy-button/index.js
+++ javascript/copy-button/index.js
@@ -1,12 +1,14 @@
-var btns = document.getElementsByClassName("copybtn");
-for (i = 0; i < btns.length; i++) { 
-  btns[i].addEventListener("click", function() {
-    let selection = window.getSelection();
-    let range = document.createRange();
-    range.selectNodeContents(this.parentElement.getElementsByClassName("copyarea")[0]);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    document.execCommand("Copy");
-    selection.removeAllRanges();
-  });
-}
+window.addEventListener('load', () => {
+  var btns = document.getElementsByClassName("copybtn");
+  for (i = 0; i < btns.length; i++) { 
+    btns[i].addEventListener("click", function() {
+      let selection = window.getSelection();
+      let range = document.createRange();
+      range.selectNodeContents(this.parentElement.getElementsByClassName("copyarea")[0]);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      document.execCommand("Copy");
+      selection.removeAllRanges();
+    });
+  }
+});
