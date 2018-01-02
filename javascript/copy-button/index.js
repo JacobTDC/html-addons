d3a74c0da37e777cb9592c7078f9680d5a63195f
JacobTDC <jacob.story.JS@gmail.com>
2018-01-02 09:24:30 -0600

    Update index.js

--- javascript/copy-button/index.js
+++ javascript/copy-button/index.js
@@ -1,15 +1,17 @@
 /*! (c) Jacob Story | github.com/JacobTDC/html-addons/blob/master/LICENSE */
-window.addEventListener('load', () => {
-  var btns = document.getElementsByClassName("copybtn");
-  for (i = 0; i < btns.length; i++) { 
-    btns[i].addEventListener("click", function() {
-      let selection = window.getSelection();
-      let range = document.createRange();
-      range.selectNodeContents(this.parentElement.getElementsByClassName("copyarea")[0]);
-      selection.removeAllRanges();
-      selection.addRange(range);
-      document.execCommand("Copy");
-      selection.removeAllRanges();
-    });
-  }
-});
+!(function() {
+  window.addEventListener('load', () => {
+    var btns = document.getElementsByClassName("copybtn");
+    for (i = 0; i < btns.length; i++) { 
+      btns[i].addEventListener("click", function() {
+        let selection = window.getSelection();
+        let range = document.createRange();
+        range.selectNodeContents(this.parentElement.getElementsByClassName("copyarea")[0]);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand("Copy");
+        selection.removeAllRanges();
+      });
+    }
+  });
+}());
